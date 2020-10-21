174f160eeeae7a97fdc7e4606ffa10a8f624a126
Matheu Campbell <47165434+matheucampbell@users.noreply.github.com>
2020-10-21 13:46:42 -0400

    Update threadsling.py
    Commented for clearer understanding

--- threadsling.py
+++ threadsling.py
@@ -1,18 +1,18 @@
-import cv2 as cv
+import cv2 as cv  # Open-source computer vision library
 import math
 import numpy as np
-import picamera
+import picamera  # Interface module for Raspberry Pi Camera
 import picamera.array
 from threading import Thread
 from time import sleep
 import wiringpi as wp
 
-game = True
-show = True
-first = True
-end = False
-det = False
-tap = False
+game = True  # Whether or not a round is currently happening
+show = True  # Whether or not the program should show the camera feed
+first = True  # Whether or not the current frame is the detection frame
+end = False  # Whether or not game has ended
+det = False  # Whether or not a detection exists for the car
+tap = False  # Whether or not the program should be tapping
 cur_rec = False
 no_rec = False
 
@@ -33,12 +33,14 @@ frames = 0
 streak = 0
 base_height = 0
 
-crit = (cv.TermCriteria_EPS | cv.TermCriteria_COUNT, 10, 1)
-null_rec = ((0.0, 0.0), (0.0, 0.0), 0.0)
+crit = (cv.TermCriteria_EPS | cv.TermCriteria_COUNT, 10, 1)  # Criteria to run CamShift function with
+null_rec = ((0.0, 0.0), (0.0, 0.0), 0.0)  # Empty rectangle placeholder
 
+# Location of cascade classifier object; used to initially detect car's location
 casc_path = '/home/pi/CV_Projects/cascades/newcascade.xml'
 casc_obj = cv.CascadeClassifier(casc_path)
 
+# Establishes PWM for micro-servo
 wp.wiringPiSetupGpio()
 wp.pinMode(18, wp.GPIO.PWM_OUTPUT)
 wp.pwmSetMode(wp.GPIO.PWM_MODE_MS)
@@ -46,7 +48,7 @@ wp.pwmSetClock(192)
 wp.pwmSetRange(2000)
 wp.pwmWrite(18, rel_val)
 
-
+# To be run in a separate thread to fetch frames
 def get_frames():
     global recent_frame
     global end
@@ -62,7 +64,7 @@ def get_frames():
                 if end:
                     break
 
-
+# To show frames
 def show():
     global show_frame
     global end
@@ -71,7 +73,7 @@ def show():
             cv.imshow('Current Frame', show_frame)
             cv.waitKey(1)
 
-
+# Returns the quadrant of a given rectangle relative to the car's rectangle
 def get_quad(center_rec, test_rec, angle):
     f_x, f_y = test_rec[0]
     rot_ang = angle
@@ -80,7 +82,7 @@ def get_quad(center_rec, test_rec, angle):
         if f_y >= center_rec[0][1]:  # fourth quadrant
             quadrant = 4
             rot_ang = 180 - rot_ang
-        else:
+        else:  # first quadrant
             quadrant = 1
     else:
         if f_y >= center_rec[0][1]:  # third quadrant
@@ -101,6 +103,7 @@ show_thread.start()
 
 sleep(1)
 print('Game Started')
+# Initial tap to start the game
 wp.pwmWrite(18, tap_val)
 sleep(.25)
 wp.pwmWrite(18, rel_val)
@@ -110,19 +113,26 @@ while game:
     frames += 1
     
     if frames <= cutoff:  # Run cascade classifier on first straightaway
-        recs = casc_obj.detectMultiScale(current_frame)
+        recs = casc_obj.detectMultiScale(current_frame)  # Detects car with a cascade classifier
         
-        if type(recs) != tuple:
+        if type(recs) != tuple:  # If cascade classifier finds a match
             recent = recs
+            # Defines search window to be used by CamShift
             search_window = (recs[0][0], recs[0][1], recs[0][2], recs[0][3])
 
+            # Creates a version of current_frame that's cropped around the bounding box of the detected
+            # rectangle in order to eventually create a mask from certain values in the cropped rectangle
             crop = current_frame[recs[0][1]:recs[0][1] + recs[0][3], recs[0][0]:recs[0][0] + recs[0][2]].copy()
             crop_hsv = cv.cvtColor(crop, cv.COLOR_BGR2HSV)
 
+            # Grabs a pixel close to the top left that's likely to be a road value and defines minimum
+            # saturation and value numbers for the mask as 50 greater than those of that pixel
             top_left = crop_hsv[0:2, 0:2]
             sat_lim = np.average(top_left[:, :, 1]) + 50
             value_lim = np.average(top_left[:, :, 2]) + 50
 
+            # Generates a 32x32 hue-saturation histogram from crop_hsv, excluding pixels deleted by the mask,
+            # then normalizes it.
             mask = cv.inRange(crop_hsv, np.array((0., sat_lim, value_lim)), np.array((180., 255., 255.,)))
             crop_hist = cv.calcHist([crop_hsv], [0, 1], mask, [32, 32], [0, 180, 0, 256])
             cv.normalize(crop_hist, crop_hist, 0., 255., cv.NORM_MINMAX)
@@ -132,13 +142,16 @@ while game:
         else:
             cur_rec = False
 
-    if det and frames > cutoff:
+    if det and frames > cutoff:  # If cascade classifier period has elapsed and there's been a detection
         # CAMShift for current frame
-        cur_hsv = cv.cvtColor(current_frame, cv.COLOR_BGR2HSV)
+        cur_hsv = cv.cvtColor(current_frame, cv.COLOR_BGR2HSV)  # Converts frame to HSV colorspace
+        # Calculates a back projection of the current frame based on the last generated histogram.
+        # On a back projection, every pixel of the current frame has a brightness that correlates
+        # with the value of the histogram bin it fits into on crop_hist.
         cur_bp = cv.calcBackProject([cur_hsv], [0, 1], crop_hist, [0, 180, 0, 256], 1)
 
-        if rec is not None and rec[0] != (0., 0.) and frames >= 50:
-            if first:
+        if rec is not None and rec[0] != (0., 0.) and frames >= 50:  # CamShift starts after 50 frames.
+            if first:  # Defines the dimensions of the rectangle to not mask out
                 height = rec[1][1] * .7
                 width = height
                 base_height = rec[1][1]
@@ -150,12 +163,16 @@ while game:
             fin_x = cen_x - (width/2)
             fin_y = cen_y - (height/2)
 
+            # Removes values from the back projection besides those in the car's detection range to increase 
+            # effectivenes of CamShift
             bp_mask[int(fin_y):int(fin_y + height), int(fin_x):int(fin_x + width)] = 255
             cur_bp = cv.bitwise_and(cur_bp, cur_bp, mask=bp_mask)
 
+        # CamShift returns a rectangle defined by a center point, width/height, and rotation angle
+        # CamShift also returns a search window
         rec, search_window = cv.CamShift(cur_bp, search_window, crit)
 
-        if rec == null_rec or rec[1][1] <= (base_height * .55):
+        if rec == null_rec or rec[1][1] <= (base_height * .55):  # If CamShift made an error, reset from last success
             no_rec = True
             if streak == 0:
                 x, y, w, h = search_window
@@ -172,6 +189,11 @@ while game:
             streak = 0
 
             # Trigonometry
+            # Generates two rectangles that are in front of and behind the car.
+            # After determining which is in the front, the program will check 
+            # if there are nonroad pixels within this rectangle to decide if it
+            # should tap the screen.
+            
             h_one = rec[1][1]
             spc = 15
 
@@ -236,6 +258,7 @@ while game:
             last_ang = rot_ang
             
             # Extract from front
+            # Takes the values from the rectangle in front of the car for analysis
             cur_rot = current_frame.copy()
             
             try:
@@ -254,6 +277,8 @@ while game:
                 break
 
             # Calculate brightness difference
+            # Large brightness difference between minimum and maximum indicates that
+            # there is road and nonroad within the front rectangle
             if rot_crop is not None:
                 rot_crop_hsv = cv.cvtColor(rot_crop, cv.COLOR_BGR2HSV)
                 val_max = rot_crop_hsv[:, :, 2].max()
